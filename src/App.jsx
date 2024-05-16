f672f89626e1e7f719ff4e5e9d83ffd6564ffeca
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-05-16 21:16:37 +0600

    adding tailwind css and daisy ui styles

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <h1 className='text-4xl font-bold text-center'>Welcome to React Router </h1>
+      
+      </div>
+   
+    </>
+  )
+}
+
+export default App
